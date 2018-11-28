d927d745d8404dcdc3e4d920cefee11123b47b83
anruly <anruly01@outlook.com>
2018-11-28 23:22:30 +0800

    Update test.py
    222

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-def recursion(i):#定义函数
-	print(i)
-	if i/2 > 1:
-		re = recursion(i/2)
-		print('返回值:',re)
-	print('上层递归值:',i)
-	return i
+def recursion(i):  # 定义函数
+    print(i)
+    if i / 2 > 1:
+        re = recursion(i / 2)
+        print('返回值:', re)
+        print('上层递归值:', i)
+    return i
+
+
 recursion(10)
